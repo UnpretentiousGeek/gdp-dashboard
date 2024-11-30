0244bdf65eca5912902fdde3869faad2da6262cf
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-11-30 23:10:32 +0000

    asv

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit as st
 from streamlit_js_eval import streamlit_js_eval, copy_to_clipboard, create_share_link, get_geolocation
 import json
 
-from reverse_geopy import GeoSearch
+from opencage.geocoder import OpenCageGeocode
+api_key = "d8c5895c69b84c54b16f3e043c02024d"
+geocoder = OpenCageGeocode(api_key)
 
 if st.checkbox("Check my location"):
     loc = get_geolocation()
@@ -11,23 +13,20 @@ if st.checkbox("Check my location"):
 geolocator = Nominatim(user_agent="location_finder")
 
 if loc:
-
-    geosearch = GeoSearch()
-
-    # Streamlit app
     st.title("City, State, and Country Finder")
 
     # Get latitude and longitude as input
     latitude = loc['coords']['latitude']
     longitude = loc['coords']['longitude']
-    
-    if latitude and longitude:
-        location = geosearch.query(lat=latitude, lon=longitude)
 
-        if location:
-            city = location.get("city", '')
-            state = location.get("state", '')
-            country = location.get("country", '')
+    if latitude and longitude:
+        result = geocoder.reverse_geocode(latitude, longitude)
+
+        if result:
+            # Extract the city, state, and country from the result
+            city = result[0]['components'].get('city', '')
+            state = result[0]['components'].get('state', '')
+            country = result[0]['components'].get('country', '')
 
             # Display the city, state, and country
             st.write(f"City: {city}")
@@ -37,3 +36,5 @@ if loc:
             st.write("Location not found. Please check the coordinates.")
     else:
         st.write("Please enter valid coordinates.")
+
+        
