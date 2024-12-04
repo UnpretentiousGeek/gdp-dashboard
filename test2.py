1a58f1a078e3e849ea577b5f833d28cea591534a
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-12-04 03:32:38 +0000

    aerg

---
+++ test2.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import folium
+from streamlit_folium import folium_static
+
+# Streamlit app
+st.title("Map")
+
+# Input fields for latitude and longitude
+latitude = st.number_input("Enter Latitude", value=0.0)
+longitude = st.number_input("Enter Longitude", value=0.0)
+
+if st.button("Show Location"):
+    location = (latitude, longitude)
+    m = folium.Map(location=location, zoom_start=13)
+    folium.Marker(location, popup="Your Location").add_to(m)
+    with st.sidebar:
+        folium_static(m, width=500, height=500)
+else:
+    st.write("Please enter latitude and longitude to display the location.")
